fda34dfa202207a0f1d2ddb7ae681446f6937cd0
lana freitas <lanadeoliveira@gmail.com>
2022-11-30 12:19:32 -0300

    before ticket

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :index, :show ]
 
   def home
+    @surveys = Survey.all
+  end
+
+  def index
+    @surveys = Survey.all
+  end
+
+  def show
+    @survey = Survey.find(params[:id])
+    @devise = Devise.new
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey.permit(:user_id))
   end
 end
