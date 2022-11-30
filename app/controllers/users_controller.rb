04f12fd242065a30dc6e5e6803fc3c00e34eb439
lana freitas <lanadeoliveira@gmail.com>
2022-11-30 15:03:41 -0300

    start

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @canditates = User.find(candidates_params[:id])
+    # @booking = Booking.new
+  end
+
+  # redirect to views/users/list_candidates where we have all candidates profiles from a user_id
+  def list_candidates
+    @users = current_user.candidates
+  end
+
+  private
+
+  def user_candidates_params
+    params.require(:user).permit(:name, :role, :state, :party, :email, :photo)
+  end
 end
