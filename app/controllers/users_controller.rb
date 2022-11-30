75f3ff755c80e2030ef899024ec0fc2c1adbcc0c
Giovanni Barros <barrosgiovanni@icloud.com>
2022-11-30 19:15:56 -0300

    add dotenv-rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  # get the top action...
+  # return the first 5 (five) candidates according to the proximity percentage between the voter and the candidate...
+  # change the name of the action and include de route after all
+  def index
+    # @users = User.where(percentage >= 80%)
+    @user = User.all
+  end
+
+  # show the user profile... left-wing or right-wing and priorities...
   def show
     @user = User.find(params[:id])
   end
