60c635eef1caba18be539babf2243ae7c3e706c0
julia-finamore <juliamm_finamore@hotmail.com>
2022-12-01 18:13:39 -0300

    everything related to read and answer questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :user_answers
   validates :name, presence: true
   validates :state, presence: true
+  has_one_attached :photo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  GET "surveys/new", to: "surveys#new"
-  POST "surveys/:id", to: "surveys#create"
-  GET "users/:id", to: "users#show"
-  GET "users/:id", to: "users#index"
-  GET "users/candidates", to: "users#index"
-  GET "devise/users", to: "users#show"
+  # GET "surveys/new", to: "surveys#new"
+  # POST "surveys/:id", to: "surveys#create"
+  # GET "users/:id", to: "users#show"
+  # GET "users/:id", to: "users#index"
+  # GET "users/candidates", to: "users#index"
+  # GET "devise/users", to: "users#show"
+  # resources :users, only: %i[index show] do
+  resources :questions
+  resources :user_answers
+
 end
