3c4518139e48a9508ac32ffb2849923888877c20
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-02 10:41:41 -0300

    some changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_180310) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_222019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,7 +81,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_180310) do
     t.string "party"
     t.string "race"
     t.string "gender"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
