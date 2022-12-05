bf295c5701c7e961bf06b488e8c46e913325ba46
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-05 11:34:43 -0300

    add link to match page and started working on profile algorithm

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -12,3 +12,31 @@ class UserAnswersController < ApplicationController
     redirect_to questions_path
   end
 end
+
+# # create a score for each one of the political orientations ...
+
+# score_left = 0
+# score_right = 0
+# score_center = 0
+
+# # iterate through each one of the user answers and assign +1 ...
+
+# UserAnswer.all.each do |answer|
+#   if answer_id.profile == 'left'
+#     score_left += 1
+#   elsif answer_id.profile == 'right'
+#     score_right += 1
+#   else
+#     score_center += 1
+#   end
+# end
+
+# # return the score for each political orientation in the profile page
+
+# if score_left > score_right && score_left > score_center
+#   # orientation = left ###### we should say that the voter has a left-wing profile
+# elsif score_right > score_left && score_right > score_center
+#   # orientation = rigth ###### we should say that the voter has a right-wing profile
+# else
+#   # orientation = center ###### we should say that the voter has a centered profile
+# end
